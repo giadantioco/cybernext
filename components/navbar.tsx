3142b5bb3b5b0bad8e8cbe46597b69603e772150
giadantioco <giada.antioco@gmail.com>
2024-08-28 17:50:48 +0200

    fix: app setup + navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { labels } from "../data/labels";
+
+interface MenuItem {
+  name: string;
+  path: string;
+}
+
+const menuList: MenuItem[] = [
+  {
+    name: labels.navProfile,
+    path: "/profile",
+  },
+  {
+    name: labels.navCreate,
+    path: "/create",
+  },
+  {
+    name: labels.navHome,
+    path: "/",
+  },
+];
+
+const Navbar: React.FC = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex items-center justify-between py-2 px-4 bg-petrol font-syne text-white">
+      <h1 className="font-bold text-xl">{labels.logoTitle}</h1>
+      <ul className="flex gap-4">
+        {menuList.map((item, index) => (
+          <li key={index}>
+            <Link
+              href={item.path}
+              className={`flex items-center ${
+                pathname === item.path ? "font-bold underline" : ""
+              }`}
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ data/labels.js
@@ -0,0 +1,29 @@
+export const labels = {
+  // client product object
+  productTableItem: "ITEM",
+  productTableEmpty: "",
+  productTableCategory: "CATEGORY",
+  productTableQuantity: "QUANTITY",
+  productTableIsbn: "ISBN",
+  productTableBtnView: "View",
+  productTableBtnEdit: "Edit",
+  productTableBtnDelete: "Delete",
+  // title in home
+  productList: "Product List",
+  // navbar content
+  logoTitle: "CYBERNEST",
+  navHome: "Product List",
+  navProductDetail: "Product Detail",
+  navCreate: "Add Product",
+  navProfile: "User Profile",
+
+  // filter title and searchbar
+  filterItem: "Filter Item",
+  filterByItem: "Filter by item",
+  // loading message
+  isLoading: "Is loading",
+  // error message
+  errorUhoh: "Uh-oh!",
+  // create page
+  createInsertNewItem: "Insert new item",
+};
