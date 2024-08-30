1a62c15cdc2a101b8592ab3f5a16b24cadaeb3ca
giadantioco <giada.antioco@gmail.com>
2024-08-30 17:07:49 +0200

    fix: add loading page (not working?)

---
+++ app/loading.tsx
@@ -0,0 +1,20 @@
+function Loading() {
+  return (
+    <div className="flex flex-col items-center">
+      {/* <iframe
+        src="https://giphy.com/embed/u2wg2uXJbHzkXkPphr/video"
+        width="480"
+        height="322"
+        className="border-none"
+        allowFullScreen
+      ></iframe>
+      <p>
+        <a href="https://giphy.com/clips/studiosoriginals-sleep-tired-sleepy-u2wg2uXJbHzkXkPphr">
+          via GIPHY
+        </a>
+      </p> */}
+      loading
+    </div>
+  );
+}
+export default Loading;
